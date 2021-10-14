7a7d33659ca298c5fb8bcdab78049c66f9203c0b
dgreipl <dgreipl@hawlandshutde.onmicrosoft.com>
2021-10-14 16:22:34 +0200

    Class after S1

--- app/src/main/java/de/hawlandshut/pluto22_ukw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto22_ukw/MainActivity.java
@@ -14,6 +14,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO: Hier noch eine Testausgabe einfügen.
-        Log.d(TAG,"in onCreate1");
+        Log.d(TAG,"in onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"in onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"in onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"in onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"in onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"in onDestroy");
     }
 }
