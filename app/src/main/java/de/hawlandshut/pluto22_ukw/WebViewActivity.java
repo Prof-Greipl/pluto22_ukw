85787a6920fefa4276badd2eb4fe00ad5a1ee479
Prof-Greipl <dieter.greipl@haw-landshut.de>
2021-11-14 17:40:51 +0100

    display html from variable

--- app/src/main/java/de/hawlandshut/pluto22_ukw/WebViewActivity.java
+++ app/src/main/java/de/hawlandshut/pluto22_ukw/WebViewActivity.java
@@ -3,6 +3,7 @@ package de.hawlandshut.pluto22_ukw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
 import android.webkit.WebView;
 
 public class WebViewActivity extends AppCompatActivity {
@@ -14,8 +15,14 @@ public class WebViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
         WebView mWebView = (WebView) findViewById(R.id.webview);
+        
 //        mWebView.loadUrl("https://archima-server.firebaseapp.com/staticpages/about.html");
-        mWebView.loadUrl("https://www.haw-landshut.de");
+        // mWebView.loadUrl("https://www.haw-landshut.de");
+        String unencodedHtml =
+                "<html><body>'%23' is the percent code for ‘#‘ </body></html>";
+        String encodedHtml = Base64.encodeToString(unencodedHtml.getBytes(),
+                Base64.NO_PADDING);
+        mWebView.loadData(encodedHtml, "text/html", "base64");
 
     }
 }
