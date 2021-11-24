99d4596cd541b072bad3c471fd03a87d2ae2b592
Prof-Greipl <dieter.greipl@haw-landshut.de>
2021-11-24 14:01:47 +0100

    Class after T4

--- app/src/main/java/de/hawlandshut/pluto22_ukw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto22_ukw/MainActivity.java
@@ -6,29 +6,101 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hawlandshut.pluto22_ukw.model.Post;
+import de.hawlandshut.pluto22_ukw.test.PostTestData;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "xxMainActivity";
 
+    // TODO: Only for for testing
+    private static final String TEST_MAIL = "dietergreipl@gmail.com";
+    private static final String TEST_PASSWORD ="123456";
+
+
+    ArrayList<Post> mPostList; // Enthält posts, die vom vom server kommen
+    ArrayAdapter<Post> mAdapter;
+
+    // UI Variable deklarieren
+    ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Testdaten erzeugen
+        // TODO: only for testing - remove later
+        PostTestData.createTestData();
+        mPostList = (ArrayList<Post>) PostTestData.postTestList;
+
+
+        mAdapter = new ArrayAdapter<Post>(
+                this,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text1,
+                mPostList
+        ){
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+                // Hole der "leere" View vom Adapter
+                View view = super.getView(position, convertView, parent);
+
+                TextView text1, text2;
+                text1 = view.findViewById( android.R.id.text1 );
+                text2 = view.findViewById( android.R.id.text2 );
+
+                Post post = getItem(position);
+
+                text1.setText(post.author);
+                text2.setText(post.title + "\n" + post.body);
+
+                return view;
+            }
+        };
+
+
+        // UI Variable initialisieren
+        mListView = findViewById( R.id.mainListViewMessages );
+
+        // ListView mit Adapter verbinden
+        mListView.setAdapter( mAdapter );
+
+
         //TODO: Hier noch eine Testausgabe einfügen.
         Log.d(TAG,"in onCreate");
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // TODO: nur zum Testen...
-        Intent intent = new Intent(getApplication(), PostActivity.class);
-        startActivity(intent);
+       // Intent intent = new Intent(getApplication(), ManageAccountActivity.class);
+       // startActivity(intent);
+
+        // Test der Authentisierung
+        // TODO: Remove later - testing only
 
         Log.d(TAG,"in onStart");
     }
@@ -43,19 +115,64 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch( item.getItemId() ){
-            case R.id.mainMenuItem1:
-                Toast.makeText(getApplicationContext(), "You pressed Item 1.", Toast.LENGTH_LONG).show();
+            case R.id.mainMenuCreateTestuser:
+                doCreateUser();
                 return true;
 
-            case R.id.mainMenuItem2:
-                Toast.makeText(getApplicationContext(), "You pressed Item 2.", Toast.LENGTH_LONG).show();
+            case R.id.mainMenuDeleteTestuser:
+                doDeleteUser();
                 return true;
-
-            case R.id.mainMenuItem3:
-                Toast.makeText(getApplicationContext(), "You pressed Item 3.", Toast.LENGTH_LONG).show();
+            
+            case R.id.mainMenuTestAuthStatus:
+                doTestAuthStatus();
+                return true;
+                
+            case R.id.mainMenuSignInTestuser:
+                doSignIn();
+                return true;
+                
+            case R.id.mainMenuSignOutTestuser:
+                doSignOut();
+                return true;
+                
+            case R.id.mainMenuSendActivationMail:
+                doSendActivationMail();
+                return true;
+            
+            case R.id.mainMenuSendResetPasswordMail:
+                doSendResetPasswordMail();
                 return true;
         }
         return true;
+    }
+
+    private void doSendResetPasswordMail() {
+    }
+
+    private void doSendActivationMail() {
+    }
+
+    private void doSignOut() {
+    }
+
+    private void doSignIn() {
+    }
+
+    private void doTestAuthStatus() {
+        FirebaseUser mUser;
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (mUser == null){
+            Toast.makeText(getApplicationContext(), "Kein User angemeldet.", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "User: " + mUser.getEmail(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void doDeleteUser() {
+    }
+
+    private void doCreateUser() {
     }
 
     // TODO: Remove later
